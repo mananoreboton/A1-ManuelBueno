cc39254d11dc55866cbed03b824d1b2ec1815d23
Manuel Bueno <mrbueno@gmail.com>
2024-12-03 21:51:41 +0100

    Read files and Create NeuralNet instance

--- NeuralNet.py
+++ NeuralNet.py
@@ -1,35 +1,37 @@
-import csv
 import random
 from PreprocessData import PreprocessData
-
+import numpy as np
 
 class NeuralNet:
-    def __init__(self, L, n, epochs=None, learning_rate=None, momentum=None, fact=None, validation_split=None):
-        """
-        Task 2: Implement a neural network with back-propagation manually.
-        """
-
-        self.L = L  # Number of layers
-        self.n = n.copy()  # Number of units in each layer
-        self.epochs = epochs  # Number of epochs
-        self.learning_rate = learning_rate  # Learning rate
-        self.momentum = momentum  # Momentum term
-        self.fact = fact  # Activation function
-        self.validation_split = validation_split  # Percentage of validation set
+    def __init__(self, L, n, n_epochs, learning_rate, momentum, activation_function, validation_split):
+        self.L = L
+        self.n = n
+        self.n_epochs = n_epochs
+        self.learning_rate = learning_rate
+        self.momentum = momentum
+        self.fact = activation_function
+        self.validation_split = validation_split
 
         # Online BP algorithm: 2- Initialize all weights and thresholds randomly
-        self.w = []
-        for i in range(L - 1):
-            self.w.append([[random.uniform(-1.0, 1.0) for _ in range(self.n[i + 1])] for _ in range(self.n[i])])
-        self.theta = []
-        for i in range(L - 1):
-            self.theta.append([random.uniform(-1.0, 1.0) for _ in range(self.n[i + 1])])
-        self.d_w_prev = []
-        for i in range(L - 1):
-            self.d_w_prev.append([[0.0 for _ in range(self.n[i + 1])] for _ in range(self.n[i])])
-        self.d_theta_prev = []
-        for i in range(L - 1):
-            self.d_theta_prev.append([0.0 for _ in range(self.n[i + 1])])
+        self.w = [None]
+        self.theta = [None]
+        for l in range(1, self.L):
+            self.w.append(np.random.randn(self.n[l], self.n[l - 1]))
+            self.theta.append(np.random.randn(self.n[l], 1))
+        self.d_w_prev = [None]
+        self.d_theta_prev = [None]
+        for l in range(1, self.L):
+            self.d_w_prev.append(np.zeros((self.n[l], self.n[l - 1])))
+            self.d_theta_prev.append(np.zeros((self.n[l], 1)))
+
+        self.h = [None] * self.L
+        self.xi = [None] * self.L
+        self.delta = [None] * self.L
+        self.d_w = [None] * self.L
+        self.d_theta = [None] * self.L
+        self.activation_function, self.activation_derivative = self.get_activation_function(self.fact)
+        self.training_errors = []
+        self.validation_errors = []
 
         print("NeuralNet initialized.")
 
@@ -43,7 +45,7 @@ class NeuralNet:
         y_train, y_val = [[0], [0]]
 
         # TODO: For epoch = 1 To num epochs
-        for epoch in range(self.epochs):
+        for epoch in range(self.n_epochs):
 
             # TODO: For pat = 1 To num training patterns
             indices = list(range(len(X_train)))
@@ -72,6 +74,32 @@ class NeuralNet:
         """
         # TODO: Return the evolution of the training and validation errors for each epoch
 
+    def get_activation_function(self, name):
+        if name == 'sigmoid':
+            def activation(x):
+                return 1 / (1 + np.exp(-x))
+            def derivative(x):
+                s = activation(x)
+                return s * (1 - s)
+        elif name == 'tanh':
+            def activation(x):
+                return np.tanh(x)
+            def derivative(x):
+                return 1 - np.tanh(x) ** 2
+        elif name == 'relu':
+            def activation(x):
+                return np.maximum(0, x)
+            def derivative(x):
+                return np.where(x > 0, 1.0, 0.0)
+        elif name == 'linear':
+            def activation(x):
+                return x
+            def derivative(x):
+                return np.ones_like(x)
+        else:
+            raise ValueError('Unsupported activation function name')
+        return activation, derivative
+
     def main(self):
         """
         Main function to execute all subtasks of Neural Network with Back-Propagation (BP).
@@ -82,20 +110,22 @@ class NeuralNet:
         self.fit()
 
 def readFile(filepath='./data/transformed_train_matrix.csv'):
-    train_matrix = []
-    with open(filepath, 'r') as file:
-        reader = csv.reader(file)
-        # Skip the header if it exists
-        next(reader, None)
-        for row in reader:
-            # print([value for value in row])
-            train_matrix.append([float(value) for value in row])
-    return train_matrix
+    data = np.genfromtxt(filepath, delimiter=',', skip_header=1)
+    return data
 
 if __name__ == "__main__":
     preprocessData = PreprocessData()
     preprocessData.select_and_analyze_dataset()
-    x = readFile()
+    data_train = readFile()
+    X_in = data_train[:, :-1]
+    y_in = data_train[:, -1]
 
-    nn = NeuralNet(4, [4, 9, 5, 1], 100)
-    nn.main()
+    nn = NeuralNet(
+        L=3,
+        n= [np.shape(X_in)[1], 10, 1],
+        n_epochs=100,
+        learning_rate=0.01,
+        momentum=0.9,
+        activation_function='tanh',
+        validation_split=0.2
+    )
